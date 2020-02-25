d5efa3bd59c4d9c6b4a2de1848a87bea3d7b16be
lccatala <lccm1@alu.ua.es>
2020-02-25 19:13:08 +0000

    first commit
    Register and Login activities, only frontend

---
+++ app/src/main/java/com/example/skoot/LoginActivity.kt
@@ -0,0 +1,25 @@
+package com.example.skoot
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
+
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        // Hide action bar
+        if (supportActionBar != null)
+            supportActionBar?.hide()
+
+        // Switch to activity_main
+        switchToRegisterText.setOnClickListener {
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/skoot/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.skoot
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Window
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.passwordText
+import kotlinx.android.synthetic.main.activity_main.registerButton
+import kotlinx.android.synthetic.main.activity_main.usernameText
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Hide action bar
+        if (supportActionBar != null)
+            supportActionBar?.hide()
+
+
+        registerButton.setOnClickListener {
+            val username = usernameText.text.toString()
+            val password = passwordText.text.toString()
+            val repeatedPassword = repeatPasswordText.text.toString()
+
+            if (password == repeatedPassword) {
+                Log.d("Debugging", "Username: $username, password: $password")
+            } else {
+                Log.d("Debugging", "Passwords  $password and $repeatedPassword are not the same!")
+            }
+        }
+
+        // Switch to activity_main
+        switchToLoginText.setOnClickListener {
+            var intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
