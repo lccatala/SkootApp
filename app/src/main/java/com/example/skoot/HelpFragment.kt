33ee826bbc9df6f8e75e7f4c687ef6a81b3265a5
lccatala <lccm1@alu.ua.es>
2020-04-16 18:15:03 +0200

    Help section

--- app/src/main/java/com/example/skoot/HelpFragment.kt
+++ app/src/main/java/com/example/skoot/HelpFragment.kt
@@ -5,7 +5,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.example.skoot.R
+import kotlinx.android.synthetic.main.layout_help.*
+import kotlinx.android.synthetic.main.layout_help.view.*
+import kotlinx.android.synthetic.main.layout_help.view.helpRentingContent
 
 class HelpFragment : Fragment() {
 
@@ -18,7 +22,13 @@ class HelpFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.layout_help, container, false)
+    }
+
+    private fun toggleText(tv: TextView) {
+        if (tv.visibility == View.VISIBLE)
+            tv.visibility = View.INVISIBLE
+        else
+            tv.visibility = View.VISIBLE
     }
 }
